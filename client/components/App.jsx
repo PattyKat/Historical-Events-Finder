80e26f616645d85973d5553abc07c78b4bbfa5b6
Patrice Cottman <patrice.cottman@gmail.com>
2021-02-17 22:39:01 -0500

    added search bar and created get request; now working on paginating and displaying results

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import styles from './styles.css';
+import Search from './Search';
 
 const App = () => {
-  const id = 'hello, from App.jsx!';
+  const [results, setresults] = useState([]);
+  const searchEvent = (query) => {
+    axios.get(`/events?q=${query}`)
+      .then((data) => console.log(data.data));
+  };
   return (
     <div className={styles.title}>
-      <p>{id}</p>
+      <Search searchEvent={searchEvent} />
     </div>
   );
 };
---
+++ client/components/Search.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles.css';
+
+const Search = (props) => {
+  const [query, setQuery] = useState('');
+  const onSubmit = (string) => {
+    props.searchEvent(string);
+  };
+  const keyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      event.stopPropagation();
+      onSubmit(query);
+      setQuery('');
+    }
+  };
+  return (
+    <div>
+      <label htmlFor="query">
+        Search Events:
+        <input id="query" type="text" value={query} onChange={(event) => setQuery(event.target.value)} onKeyDown={keyDown} />
+      </label>
+    </div>
+
+  );
+};
+
+export default Search;
+
+Search.propTypes = {
+  searchEvent: PropTypes.func.isRequired,
+};
